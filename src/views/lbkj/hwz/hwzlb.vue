ed29ddd84dd0fae80c19e8f2eb26f87608591631
heweizhuang <1058104620@qq.com>
2022-05-09 16:05:07 +0800

    添加lbkj模板

---
+++ src/views/lbkj/hwz/hwzlb.vue
@@ -0,0 +1,36 @@
+<template>
+  <div>
+    {{msg}}
+  </div>
+</template>
+
+<script>
+  import {getAction} from '@/api/manage'
+
+  export default {
+    name: 'hwzlb',
+    data(){
+      return{
+        msg: ""
+      }
+    },
+    methods: {
+      wz(){
+        let url = "/lbkj/hwz"
+        getAction(url).then((res) => {
+          //console.log("返回数据",res)
+          if(res.success){
+            this.msg = res.result;
+          }
+        })
+      }
+    },
+    created(){
+      this.wz();
+    }
+  }
+</script>
+
+<style scoped>
+
+</style>
